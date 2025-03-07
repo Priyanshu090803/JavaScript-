16ce2bd61ea2b64df3db9a8736cff412be22c83b
Priyanshu090803 <priyanshu122a@gmail.com>
2025-03-07 22:33:03 +0530

    4march

--- Dom.js
+++ Dom.js
@@ -1,6 +1,23 @@
 let input=document.querySelector(".input");
 let clickBtn=document.querySelector(".btn");
+let todo=document.querySelector(".todo");
+
 
 clickBtn.addEventListener("click",()=>{
-    console.log(input.value)
+    let para=document.createElement('p');   
+    // let btn=document.createElement('button');
+    todo.append(para)
+    // todo.append(btn)
+    para.textContent=input.value;
+    // btn.textContent="delete"
+
+    input.value="";
+    // btn.addEventListener('click',()=>{
+    //     btn.remove()
+    //     para.remove();
+    // })
+    todo.addEventListener("click",(event)=>{
+        event.target.remove();
+    })
+
 })
---
+++ Dom2.js
@@ -0,0 +1,28 @@
+let box=document.querySelector(".box");
+let btn=document.querySelector(".btn");
+let input=document.querySelector(".input")
+let para = document.querySelector(".para")
+// btn.addEventListener("click",()=>{
+//         box.classList.add("transition")
+// })
+
+// const handlePress=(event)=>{
+//         console.log(event.code)
+//         console.log(event.key)
+//         console.log(event.type)
+// document.querySelector(".para").textContent=event.key
+
+// }
+
+// input.addEventListener('keydown',(event)=>{
+//         console.log(event.code)
+// document.querySelector(".para").textContent=event.key
+
+// })
+
+input.addEventListener("input",(event)=>{
+        console.log(event.target.value)
+        console.log(event.target.name)
+        console.log(event.inputType)
+        console.log(event.type)
+})
