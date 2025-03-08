e92e7751649345d68ac025a9e4dcb3c80d8a0339
Priyanshu090803 <priyanshu122a@gmail.com>
2025-03-08 22:42:30 +0530

    8march

---
+++ 1/script.js
@@ -0,0 +1,13 @@
+let left=document.querySelector(".left");
+let right=document.querySelector(".right");
+let copyCode=document.querySelector(".copyCode");
+
+
+
+let color=()=>{
+    
+}
+
+left.addEventListener("click",color);
+right.addEventListener("click",color);
+
